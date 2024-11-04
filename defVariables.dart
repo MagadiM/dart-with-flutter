3d59acabad88faf4ca09c9fca7a4eca06636621e
MagadiM <milliemagadi4@gmail.com>
2024-11-04 19:20:15 +0300

    Starting with dart variables

---
+++ defVariables.dart
@@ -0,0 +1,13 @@
+void main(){
+    String name = "Black Panther";
+    int yearsInExistence = 102;
+    double technologyLevel = 10.5;
+    bool isWakandaProtector = true;
+    List powerfulAttributes = ["lethal", "fast","agile", "fair"];
+
+    print("Name: $name");
+    print("Years In Existence: $yearsInExistence");
+    print("Technology Level: $technologyLevel");
+    print("Wakanda Protector: $isWakandaProtector");
+    print(" Attributes Discovered: $powerfulAttributes");
+}
