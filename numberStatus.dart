ce788de84e11c01e6529bfbf59ca5b0e3b3541c4
MagadiM <milliemagadi4@gmail.com>
2024-11-04 20:10:28 +0300

    Adding two files to the repository

---
+++ numberStatus.dart
@@ -0,0 +1,11 @@
+void main(){
+    var num1 = 0.02;
+
+    if (num1 > 0){
+        print("The number is positive.");
+    } else if (num1 < 0 ) {
+        print("The number is negative.");
+    } else {
+        print("The number is zero.");
+    }
+}
---
+++ typeConversion.dart
@@ -0,0 +1,7 @@
+void main(){
+    String callCode = '456';
+    int digit = int.parse(callCode);
+    double decimalCode = digit.toDouble();
+    print(digit);
+    print(decimalCode);
+}
